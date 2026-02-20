f991c507da441755843ab5eb217b9cef41d1e312
Anosmish <146194026+Anosmish@users.noreply.github.com>
2026-02-21 00:53:19 +0530

    Update auth.js

--- EXAM_GURU_UP/backend/exam-guru-up/routes/auth.js
+++ EXAM_GURU_UP/backend/exam-guru-up/routes/auth.js
@@ -16,6 +16,7 @@ const { verifyToken, isAdmin } = require("../middleware/authMiddleware");
 
 const transporter = nodemailer.createTransport({
     service: "gmail",
+    port:587,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
