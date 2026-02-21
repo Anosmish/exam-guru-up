02f7ce59ab62370586454474fc5424cc35a104e7
Anomish <7mr.anonymous@gmail.com>
2026-02-21 16:29:51 +0530

    27th commit

--- EXAM_GURU_UP/frontend/index.js
+++ EXAM_GURU_UP/frontend/index.js
@@ -1,11 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(){
-    const hamburger = document.getElementById("hamburger");
-    const mobileMenu = document.getElementById("mobileMenu");
-
-    hamburger.addEventListener("click", function(){
-        mobileMenu.classList.toggle("active");
-    });
-
+    
     window.toggleMenu = function(){
         document.getElementById("mobileMenu").classList.toggle("active");
     };
