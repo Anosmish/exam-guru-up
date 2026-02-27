dbd6000ee3a4d30478a708f6146af699cd5de498
Anomish <7mr.anonymous@gmail.com>
2026-02-27 18:11:27 +0530

    64th  commit

--- EXAM_GURU_UP/backend/exam-guru-up/routes/auth.js
+++ EXAM_GURU_UP/backend/exam-guru-up/routes/auth.js
@@ -243,10 +243,17 @@ router.post("/verify/:token", async (req, res) => {
             process.env.JWT_SECRET,
             { expiresIn: "7d" }
         );
+        res.cookie("token", token, {
+  httpOnly: true,
+  secure: false,
+  sameSite: "lax",
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
+
+res.redirect(process.env.FRONTEND_URL);
 
         res.send(`
             <h2>Account Verified Successfully ✅</h2>
-            <button onclick="window.location='${process.env.FRONTEND_URL}/login-success.html?token=${token}'"
                 style="padding:10px 20px;background:#16a34a;color:white;border:none;border-radius:5px;">
                 Login Now
             </button>
@@ -288,8 +295,14 @@ router.post("/login", async (req, res) => {
             { expiresIn: "7d" }
         );
 
-        res.json({ token, user });
+        res.cookie("token", token, {
+         httpOnly: true,
+        secure: false, // production me true karna
+         sameSite: "lax",
+         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        });
 
+        res.json({ user });
     } catch (err) {
         console.error("Login Error:", err); // <-- exact error console
         res.status(500).json({ message: "Login failed", error: err.message });
